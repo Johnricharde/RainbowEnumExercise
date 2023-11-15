9c0cad1848dd2b3cbc4c60b36d411be58b8cfbbd
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-15 20:15:11 +0100

    Add project files.

---
+++ RainbowEnumExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+public enum rainbow : byte
+{
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    indigo,
+    violet,
+}
+
+namespace RainbowEnumExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here's a rainbow:");
+            showRainbow();
+        }
+        static void showRainbow()
+        {
+            foreach (var color in Enum.GetValues(typeof(rainbow)))
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
